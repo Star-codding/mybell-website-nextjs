c63ee4817940ab5e9247f39274efa62aa7aec350
Jitendra Prajapat <jituprajapat8888@gmail.com>
2025-07-14 11:37:56 +0530

    updated files

--- src/app/comman/Footer.jsx
+++ src/app/comman/Footer.jsx
@@ -4,36 +4,36 @@ export default function Footer() {
   return (
     <>
       <footer
-        class="mx-auto w-full max-w-[1200px] justify-between pb-10 flex flex-col lg:flex-row"
+        className="mx-auto w-full max-w-[1200px] justify-between pb-10 flex flex-col lg:flex-row"
       >
-        <div class="ml-5">
+        <div className="ml-5">
           <img
-            class="mt-10 mb-5"
+            className="mt-10 mb-5"
             src="https://tailwind-ecommerce-demo.vercel.app/company-logo.4e8f774b.svg"
             alt="company logo"
           />
-          <p class="pl-0">
+          <p className="pl-0">
             Lorem ipsum dolor sit amet consectetur <br />
             adipisicing elit.
           </p>
-          <div class="mt-10 flex gap-3">
+          <div className="mt-10 flex gap-3">
             <a href="">
               <img
-                class="h-5 w-5 cursor-pointer"
+                className="h-5 w-5 cursor-pointer"
                 src="https://tailwind-ecommerce-demo.vercel.app/github.a6fc909a.svg"
                 alt="github icon"
               />
             </a>
             <a href="https://t.me/b_bulakh">
               <img
-                class="h-5 w-5 cursor-pointer"
+                className="h-5 w-5 cursor-pointer"
                 src="https://tailwind-ecommerce-demo.vercel.app/telegram.8c11bb1d.svg"
                 alt="telegram icon"
               />
             </a>
             <a href="https://www.linkedin.com/in/bogdan-bulakh-393284190/">
               <img
-                class="h-5 w-5 cursor-pointer"
+                className="h-5 w-5 cursor-pointer"
                 src="https://tailwind-ecommerce-demo.vercel.app/linkedin.48c56177.svg"
                 alt="twitter icon"
               />
@@ -41,10 +41,10 @@ export default function Footer() {
           </div>
         </div>
 
-        <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
-          <div class="mx-5 mt-10">
-            <p class="font-medium text-gray-500">FEATURES</p>
-            <ul class="text-sm leading-8">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+          <div className="mx-5 mt-10">
+            <p className="font-medium text-gray-500">FEATURES</p>
+            <ul className="text-sm leading-8">
               <li><a href="#">Marketing</a></li>
               <li><a href="#">Commerce</a></li>
               <li><a href="#">Analytics</a></li>
@@ -52,9 +52,9 @@ export default function Footer() {
             </ul>
           </div>
 
-          <div class="mx-5 mt-10">
-            <p class="font-medium text-gray-500">SUPPORT</p>
-            <ul class="text-sm leading-8">
+          <div className="mx-5 mt-10">
+            <p className="font-medium text-gray-500">SUPPORT</p>
+            <ul className="text-sm leading-8">
               <li><a href="#">Pricing</a></li>
               <li><a href="#">Docs</a></li>
               <li><a href="#">Audition</a></li>
@@ -62,9 +62,9 @@ export default function Footer() {
             </ul>
           </div>
 
-          <div class="mx-5 mt-10">
-            <p class="font-medium text-gray-500">DOCUMENTS</p>
-            <ul class="text-sm leading-8">
+          <div className="mx-5 mt-10">
+            <p className="font-medium text-gray-500">DOCUMENTS</p>
+            <ul className="text-sm leading-8">
               <li><a href="#">Terms</a></li>
               <li><a href="#">Conditions</a></li>
               <li><a href="#">Privacy</a></li>
@@ -72,9 +72,9 @@ export default function Footer() {
             </ul>
           </div>
 
-          <div class="mx-5 mt-10">
-            <p class="font-medium text-gray-500">DELIVERY</p>
-            <ul class="text-sm leading-8">
+          <div className="mx-5 mt-10">
+            <p className="font-medium text-gray-500">DELIVERY</p>
+            <ul className="text-sm leading-8">
               <li><a href="#">List of countries</a></li>
               <li><a href="#">Special information</a></li>
               <li><a href="#">Restrictions</a></li>
@@ -85,24 +85,24 @@ export default function Footer() {
       </footer>
 
 
-      <section class="h-11 bg-amber-400">
+      <section className="h-11 bg-amber-400">
         <div
-          class="mx-auto flex max-w-[1200px] items-center justify-between px-4 pt-2"
+          className="mx-auto flex max-w-[1200px] items-center justify-between px-4 pt-2"
         >
           <p>&copy; Bogdan Bulakh, 2023</p>
-          <div class="flex items-center space-x-3">
+          <div className="flex items-center space-x-3">
             <img
-              class="h-8"
+              className="h-8"
               src="https://cdn-icons-png.flaticon.com/512/5968/5968299.png"
               alt="Visa icon"
             />
             <img
-              class="h-8"
+              className="h-8"
               src="https://cdn-icons-png.flaticon.com/512/349/349228.png"
               alt="AE icon"
             />
             <img
-              class="h-8"
+              className="h-8"
               src="https://cdn-icons-png.flaticon.com/512/5968/5968144.png"
               alt="Apple pay icon"
             />
